8c959c45ee1339320bed79361c7e9310626edbd1
Apratim Bajpai <apri4u@gmail.com>
2023-12-08 14:29:01 -0500

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -12,6 +12,3 @@ logging.basicConfig(filename = LOG_FILE_PATH,
                     level = logging.INFO,
                     format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
-
-if __name__ == "__main__":
-    logging.info("Logging has started!")
